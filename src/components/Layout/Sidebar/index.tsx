42d1dcc45556b3bb4e756a9dc21395ffc7f0d50e
cyb3rgh05t <flammang.yves@gmail.com>
2024-03-16 12:44:01 +0100

    update Slider Logo

--- src/components/Layout/Sidebar/index.tsx
+++ src/components/Layout/Sidebar/index.tsx
@@ -150,7 +150,7 @@ const Sidebar = ({ open, setClosed }: SidebarProps) => {
                     <div className="flex flex-shrink-0 items-center px-2">
                       <span className="px-4 text-xl text-gray-50">
                         <a href="/">
-                          <img src="/logo_full.svg" alt="Logo" />
+                          <img src="/logo.png" alt="Logo" />
                         </a>
                       </span>
                     </div>
@@ -220,7 +220,7 @@ const Sidebar = ({ open, setClosed }: SidebarProps) => {
               <div className="flex flex-shrink-0 items-center">
                 <span className="px-4 text-2xl text-gray-50">
                   <a href="/">
-                    <img src="/logo_full.svg" alt="Logo" />
+                    <img src="/logo.png" alt="Logo" />
                   </a>
                 </span>
               </div>
