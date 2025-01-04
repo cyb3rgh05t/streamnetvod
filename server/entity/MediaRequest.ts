cb2356714866be63800ac96b91b3fa235e7a0088
cyb3rgh05t <cyb3rgh05t_01@proton.me>
2025-01-04 22:48:54 +0100

    fix lang from "fr" to "de"

--- server/entity/MediaRequest.ts
+++ server/entity/MediaRequest.ts
@@ -1199,7 +1199,7 @@ export class MediaRequest {
       }
 
       if (this.type === MediaType.MOVIE) {
-        const movie = await tmdb.getMovie({ movieId: media.tmdbId, language: 'fr' });
+        const movie = await tmdb.getMovie({ movieId: media.tmdbId, language: 'de' });
         notificationManager.sendNotification(type, {
           media,
           request: this,
@@ -1218,7 +1218,7 @@ export class MediaRequest {
           image: `https://image.tmdb.org/t/p/w600_and_h900_bestv2${movie.poster_path}`,
         });
       } else if (this.type === MediaType.TV) {
-        const tv = await tmdb.getTvShow({ tvId: media.tmdbId, language: 'fr' });
+        const tv = await tmdb.getTvShow({ tvId: media.tmdbId, language: 'de' });
         notificationManager.sendNotification(type, {
           media,
           request: this,
