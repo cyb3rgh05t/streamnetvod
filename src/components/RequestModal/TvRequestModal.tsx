cd178c86cbce9644095284863e503270a5f58007
cyb3rgh05t <cyb3rgh05t_01@proton.me>
2025-07-09 06:19:07 +0200

    fix2

--- src/components/RequestModal/TvRequestModal.tsx
+++ src/components/RequestModal/TvRequestModal.tsx
@@ -639,7 +639,7 @@ const TvRequestModal = ({
                           </td>
                           <td className="whitespace-nowrap px-1 py-4 text-sm font-medium leading-5 text-gray-100 md:px-6">
                             {season.seasonNumber === 0
-                              ? intl.formatMessage(globalMessages.extras)
+                              ? intl.formatMessage(messages.specials)
                               : intl.formatMessage(messages.seasonnumber, {
                                   number: season.seasonNumber,
                                 })}
